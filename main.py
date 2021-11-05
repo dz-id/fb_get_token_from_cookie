45e5b640dc65dc737029b37fb768121fcdb537e0
D <52864765+dz-id@users.noreply.github.com>
2021-11-05 22:58:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import requests, re
 print('\n** < GET FB ACCESS TOKEN FROM COOKIE > **\n')
 cookie = input('* Cookie? : ')
 try:
-    data = requests.get('https://m.facebook.com/composer/ocelot/async_loader/?publisher=feed#_=_', headers = {
+    data = requests.get('https://business.facebook.com/business_locations', headers = {
         'user-agent'                : 'Mozilla/5.0 (Linux; Android 8.1.0; MI 8 Build/OPM1.171019.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.86 Mobile Safari/537.36', # don't change this user agent.
-        'referer'                   : 'https://m.facebook.com/',
-        'host'                      : 'm.facebook.com',
-        'origin'                    : 'https://m.facebook.com',
+        'referer'                   : 'https://www.facebook.com/',
+        'host'                      : 'business.facebook.com',
+        'origin'                    : 'https://business.facebook.com',
         'upgrade-insecure-requests' : '1',
         'accept-language'           : 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',
         'cache-control'             : 'max-age=0',
@@ -21,7 +21,7 @@ try:
     }, cookies = {
         'cookie'                    : cookie
     })
-    find_token = re.search('(EAAA\w+)', data.text)
+    find_token = re.search('(EAAG\w+)', data.text)
     results    = '\n* Fail : maybe your cookie invalid !!' if (find_token is None) else '\n* Your fb access token : ' + find_token.group(1)
 except requests.exceptions.ConnectionError:
     results    = '\n* Fail : no connection here !!'
